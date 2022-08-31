7cefbb23648d18a48a008d877a33b58a0aaee10c
Shaquille Mandy <shaqmandy@gmail.com>
2022-08-31 14:02:57 -0700

    created dummy ActionsPrompt, game while loop

--- src/com/ericsHouse/EricHouseClient.java
+++ src/com/ericsHouse/EricHouseClient.java
@@ -1,5 +1,6 @@
 package com.ericsHouse;
 
+import com.ericsHouse.prompts.ActionsPrompt;
 import com.ericsHouse.rooms.Rooms;
 
 import java.lang.reflect.Method;
@@ -11,12 +12,6 @@ public class EricHouseClient {
 
     // ask questions about AB145-148
     public static void main(String[] args) throws NoSuchMethodException {
-        // AB#133 On game start, display title screen prompt
-
-        // AB#134 introduce the game to the players
-
-        // AB#137-140  player can get help by typing "help" or "h" / this will display the commands
-
         // AB#139 & 142 & 143 players can make choices e.g. ask shaq, inspect left/right, etc
         Map<Character, Method> actions = new HashMap<>();
         actions.put('1', Rooms.class.getMethod("inspectLeft"));
@@ -25,8 +20,19 @@ public class EricHouseClient {
         actions.put('4', Rooms.class.getMethod("moveToNextRoom"));
         actions.put('5', Rooms.class.getMethod("askShaq"));
 
-        // AB#141 player should be able to see where they currently are, display the challenge
+        while (true) {
+            // AB#133 On game start, display title screen prompt
 
-        // AB#135-136 when the game is over, ask if the player wants to play again or quit
+            // AB#134 introduce the game to the players
+
+            // AB#137-140  player can get help by typing "help" or "h" / this will display the commands
+
+
+            ActionsPrompt.actionsPrompt();
+
+            // AB#141 player should be able to see where they currently are, display the challenge
+
+            // AB#135-136 when the game is over, ask if the player wants to play again or quit
+        }
     }
 }
---
+++ src/com/ericsHouse/prompts/ActionsPrompt.java
@@ -0,0 +1,11 @@
+package com.ericsHouse.prompts;
+
+public class ActionsPrompt {
+    public static void actionsPrompt() {
+        System.out.println("1 ) Inspect Left");
+        System.out.println("2 ) Inspect Right");
+        System.out.println("3 ) Inspect Floor");
+        System.out.println("4 ) Move To Next Room");
+        System.out.println("5 ) Ask Shaq");
+    }
+}
