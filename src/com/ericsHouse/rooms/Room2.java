0bc771417a03dad3ec42048fcb29b852ccd8aa7b
Agustin <agustinduranse@gmail.com>
2022-09-14 14:56:07 -0700

    Refactored parser, AB#1054

--- src/com/ericsHouse/rooms/Room2.java
+++ src/com/ericsHouse/rooms/Room2.java
@@ -3,8 +3,9 @@ package com.ericsHouse.rooms;
 import com.ericsHouse.EricHouseClient;
 import com.ericsHouse.characters.David;
 import com.ericsHouse.jsonParser.ActionsPrompt;
-import com.ericsHouse.jsonParser.RoomTwoParser;
+import com.ericsHouse.jsonParser.RoomZeroParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,14 +15,14 @@ import java.util.Scanner;
 public class Room2 {
 
     public static String room = "RoomTwo";
-
-    public static String roomName = RoomTwoParser.getName(room);
+    public static RoomZeroParser currentRoom = new RoomZeroParser("roomZeroPrompts.json");
     static ArrayList<String> doorInventory = new ArrayList<>();
-    public static ArrayList roomItems = new ObjectMapper().convertValue(RoomTwoParser.getItems(room), ArrayList.class);
+    public static String roomName = currentRoom.getName(room);
+    public static ArrayNode items = currentRoom.getItems(room);
+    public static ArrayList roomItems = new ObjectMapper().convertValue(currentRoom.getItems(room), ArrayList.class);
 
     public static void gameLogic() throws IOException {
-        RoomTwoParser.getPrompt("enterRoom");
-
+        currentRoom.getPrompt("enterRoom");
         playerAction();
     }
 
@@ -62,18 +63,18 @@ public class Room2 {
 
     public static void inspectLeft() throws IOException {
         if (roomItems.contains("Onion")) {
-            RoomTwoParser.getPrompt("inspectLeft");
+            currentRoom.getPrompt("inspectLeft");
             David.addBackpack("Onion");
             roomItems.remove("Onion");
         } else {
-            RoomTwoParser.getPrompt("inspectLeftEmpty");
+            currentRoom.getPrompt("inspectLeftEmpty");
         }
         playerAction();
     }
 
     public static void inspectRight() throws IOException {
         if (roomItems.contains("Pencil")) {
-            RoomTwoParser.getPrompt("inspectRight");
+            currentRoom.getPrompt("inspectRight");
             David.addBackpack("Hammer");
             David.addBackpack("Pencil");
             David.addBackpack("Emerald");
@@ -81,18 +82,18 @@ public class Room2 {
             roomItems.remove("Emerald");
             roomItems.remove("Hammer");
         } else {
-            RoomTwoParser.getPrompt("inspectRightEmpty");
+            currentRoom.getPrompt("inspectRightEmpty");
         }
         playerAction();
     }
 
     public static void inspectFloor() throws IOException {
         if (roomItems.contains("Needle")) {
-            RoomTwoParser.getPrompt("inspectFloor");
+            currentRoom.getPrompt("inspectFloor");
             David.addBackpack("Needle");
             roomItems.remove("Needle");
         } else {
-            RoomTwoParser.getPrompt("inspectFloorEmpty");
+            currentRoom.getPrompt("inspectFloorEmpty");
         }
         playerAction();
     }
@@ -102,32 +103,32 @@ public class Room2 {
                 && David.getBackpack().contains("Pencil")
                 && David.getBackpack().contains("Emerald")
                 && David.getBackpack().contains("Needle")) {
-            RoomTwoParser.getPrompt("openDoorLockedWithItems");
+            currentRoom.getPrompt("openDoorLockedWithItems");
             addItemToDoor();
             if (doorInventory.indexOf("Onion") == 0
                     && doorInventory.indexOf("Pencil") == 1
                     && doorInventory.indexOf("Emerald") == 2
                     && doorInventory.indexOf("Needle") == 3) {
-                RoomTwoParser.getPrompt("openDoorUnlocked");
+                currentRoom.getPrompt("openDoorUnlocked");
             } else {
                 EricHouseClient.ericAppearsCheck();
                 doorInventory.clear();
-                RoomTwoParser.getPrompt("openDoorLockedIncorrectOrder");
+                currentRoom.getPrompt("openDoorLockedIncorrectOrder");
                 playerAction();
             }
         } else {
-            RoomTwoParser.getPrompt("openDoorLocked");
+            currentRoom.getPrompt("openDoorLocked");
             playerAction();
         }
     }
 
     public static void askShaq() throws IOException {
-        RoomTwoParser.getPrompt("askShaq");
+        currentRoom.getPrompt("askShaq");
         playerAction();
     }
 
     public static void invalidCommand() {
-        RoomTwoParser.getPrompt("invalidCommand");
+        currentRoom.getPrompt("invalidCommand");
     }
 
     public static void addItemToDoor() {
