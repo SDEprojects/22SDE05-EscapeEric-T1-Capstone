a134d0978c788ae41c2c33a8cb6b30a447824523
Katy Fernelius <kkin001@gmail.com>
2022-09-22 12:29:52 -0500

    Committing to pull changes from dev and merge AB#1141

--- src/com/ericsHouse/view/util/WordOrder.java
+++ src/com/ericsHouse/view/util/WordOrder.java
@@ -4,6 +4,7 @@ import com.ericsHouse.jsonParser.JsonParser;
 import com.ericsHouse.view.panels.GamePanel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static com.ericsHouse.view.util.KeyHandler.objIndex;
@@ -34,8 +35,12 @@ public class WordOrder {
                     gp.ui.currentDialogue = "The dog accepts that you know how to spell 'OPEN'\nHe grumbles and walks away.";
                     //TODO - figure out how to make this dynamic rather than hardcoding the array position numbers
                     gp.obj[23] = null;
+                    if(clickedObJ.size() == 4){
+                        clickedObJ.clear();
+                    }
             }else{
-                gp.ui.currentDialogue = String.format("That's Correct I need %s more items from you.", 4 - clickedObJ.size());
+                gp.ui.currentDialogue = String.format("That's Correct I need %s more items from you.\n" +
+                        "Current Items:%s", 4 - clickedObJ.size(), clickedObJ);
             }
 
         }else{
